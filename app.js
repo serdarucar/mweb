2130e269a743db05ecdafd468cef624df7891cda
Serdar <serdarn@me.com>
2015-04-26 14:55:54 +0300

    ReDB-dash added.

--- app.js
+++ app.js
@@ -3,7 +3,16 @@
 
 var express = require('express')
   , app = express()
-  , r = require('rethinkdb')
+  , r  = require('rethinkdbdash')({
+      servers: [
+        {host: '10.131.166.209', port: 28015},
+        {host: '10.131.166.209', port: 28016},
+        {host: '10.131.166.238', port: 28015},
+        {host: '10.131.166.238', port: 28016}
+      ],
+      buffer: 500,
+      max: 5000
+    })
   , debug = require('debug')('smw.tashimasu.info')
   , path = require('path')
   , cookieParser = require('cookie-parser')
@@ -27,50 +36,42 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/u', function (req, res) {
-  r.connect({ db: 'mailsender' }).then(function(c) {
-    r.table('session')
-      .filter(function(session) {
-        return session('time')
+  r.db('mailsender').table('session')
+    .filter(function(session) {
+      return session('time')
+        .inTimezone('+03')
+        .date()
+        .eq(r.now()
           .inTimezone('+03')
-          .date()
-          .eq(r.now()
-            .inTimezone('+03')
-            .date())
-      })
-      .without('mail')
-      .coerceTo('array')
-      .orderBy(r.desc('time'))
-      .run(c).then(function (result) {
-        res.render('index', { result: result });
-      })
-    .finally(function() {
-      c.close();
-    });
-  });
+          .date())
+    })
+    .without('mail')
+    .coerceTo('array')
+    .orderBy(r.desc('time'))
+    .run().then(function (result) {
+      res.render('index', { result: result });
+    })
+  .error();
 });
 
 app.get('/s/:sid', function (req, res) {
   var s_sid = req.params.sid;
-  r.connect({ db: 'mailsender' }).then(function(c) {
-    r.table('session')
-      .get(s_sid)('mail')
-      .eqJoin(function(uid) {
-        return uid;
-      }, r.table('mail')).zip()
-      .group('status').count()
-      .ungroup().map(function(doc) {
-        return {
-          status: doc('group'),
-          count: doc('reduction')
-        }
-      }).orderBy(r.desc('status'))
-        .run(c).then(function (result) {
-          res.render('session', { result: result, sid: s_sid });
-      })
-    .finally(function() {
-      c.close();
-    });
-  });
+  r.db('mailsender').table('session')
+    .get(s_sid)('mail')
+    .eqJoin(function(uid) {
+      return uid;
+    }, r.table('mail')).zip()
+    .group('status').count()
+    .ungroup().map(function(doc) {
+      return {
+        status: doc('group'),
+        count: doc('reduction')
+      }
+    }).orderBy(r.desc('status'))
+      .run().then(function (result) {
+        res.render('session', { result: result, sid: s_sid });
+    })
+  .error();
 });
 
 app.get('/m/:sid/:st/:idx', function (req, res) {
@@ -79,26 +80,22 @@ app.get('/m/:sid/:st/:idx', function (req, res) {
   var n_idx = parseInt(req.params.idx);
   var n_idx1 = math.subtract(n_idx, 50);
   var n_idx2 = math.add(n_idx, 50);
-  r.connect({ db: 'mailsender' }).then(function(c) {
-    r.table("session")
-      .get(s_sid)('mail')
-      .filter({st: s_st})
-      .pluck('addr').orderBy('addr')
-      .slice(n_idx, n_idx2)
-    .run(c).then(function (result) {
-      res.render('mails', {
-        result: result,
-        sid: s_sid,
-        st: s_st,
-        idx: n_idx,
-        idx1: n_idx1,
-        idx2: n_idx2
-      });
-    })
-    .finally(function() {
-      c.close();
+  r.db('mailsender').table("session")
+    .get(s_sid)('mail')
+    .filter({st: s_st})
+    .pluck('addr').orderBy('addr')
+    .slice(n_idx, n_idx2)
+  .run().then(function (result) {
+    res.render('mails', {
+      result: result,
+      sid: s_sid,
+      st: s_st,
+      idx: n_idx,
+      idx1: n_idx1,
+      idx2: n_idx2
     });
-  });
+  })
+  .error();
 });
 
 app.set('port', process.env.PORT || 3300);
